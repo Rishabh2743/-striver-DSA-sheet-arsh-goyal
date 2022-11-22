961bfcfe0d066b585c94538bfe921b7403f4fe75
Rishabh2743 <76483917+Rishabh2743@users.noreply.github.com>
2022-11-22 22:30:13 +0530

    Time: 20 ms (11.24%), Space: 13.7 MB (44.69%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,22 @@
 class Solution {
+private:
+    void generate(vector<string>&ans, string p, int n, int m){
+        if(n ==0 and m ==0){
+            ans.push_back(p);
+            return;
+        }
+        
+        if(n > 0)
+            generate(ans, p + '(', n-1, m+1);
+        if(m > 0)
+            generate(ans, p + ')', n, m-1); 
+    }
 public:
     vector<string> generateParenthesis(int n) {
-         vector <string> res;
-        string temp;
-        function <void(int, int)> bt = [&](int l, int r) {
-            if (l == n && r == n) res.push_back(temp);
-            else {
-                if (l < n) {
-                    temp.push_back('(');
-                    bt(l + 1, r);
-                    temp.pop_back();
-                }
-                if (l > r) {
-                    temp.push_back(')');
-                    bt(l, r + 1);
-                    temp.pop_back();
-                }
-            }
-        };
-        
-        bt(0, 0);
-        return res;
+        vector<string> ans;
+        string p = "";
+        generate(ans,p,n,0);
+        return ans;
     }
-};
-        
+};      
     
