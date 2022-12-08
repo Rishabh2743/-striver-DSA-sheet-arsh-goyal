47efd195b851b86bc5d86e43007c2936bd2fea36
Rishabh2743 <76483917+Rishabh2743@users.noreply.github.com>
2022-12-08 22:36:32 +0530

    Time: 26 ms (87.08%), Space: 13.9 MB (60.62%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>adj[numCourses];
+	        for(auto it: prerequisites){
+	            adj[it[1]].push_back(it[0]);
+	    }
+        vector<int> ind(numCourses,0);
+       for(int i=0;i<numCourses;i++){
+           for(auto it : adj[i]){
+               ind[it]++;
+           }      
+         } 
+        
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(ind[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                ind[it]--;
+                if(ind[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if (topo.size()==numCourses) return true;
+        return false;
+        
+    }
+};
