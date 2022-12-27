1175a2426ed3ff9ee833c682cde69016467cf8d9
Rishabh2743 <76483917+Rishabh2743@users.noreply.github.com>
2022-12-27 21:56:32 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,26 +8,29 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int kno(int W, int wt[], int val[], int i,vector<vector<int>>& dp){
-        if(i==0){
-            if(wt[i]<=W) return val[0];
-            else return 0;
-        }
-         if(dp[i][W]!=-1)
-        return dp[i][W];
-        
-    int notTaken = 0 + kno(W,wt,val,i-1,dp);
     
-    int taken = INT_MIN;
-    if(wt[i] <= W)
-        taken = val[i] + kno(W-wt[i],wt,val,i-1,dp);
-                          
-    return dp[i][W] = max(notTaken,taken);
-    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        vector<vector<int>>dp(n,vector<int>(W+1,-1));
-        return kno(W,wt,val,n-1,dp);
+        vector<vector<int>> dp(n,vector<int>(W+1,0));
+          for(int i=wt[0]; i<=W; i++){
+        dp[0][i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + dp[ind-1][cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                
+            dp[ind][cap] = max(notTaken, taken);
+        }
+    }
+    
+    return dp[n-1][W];
+
        // Your code here
     }
 };
