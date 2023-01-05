a2edafcbf41ca5fd936270874aa698655a54cba8
Rishabh2743 <76483917+Rishabh2743@users.noreply.github.com>
2023-01-05 19:59:03 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (54.40%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,15 +5,11 @@ public:
         while(i<matrix.size() && j< matrix[0].size()){
             if(matrix[i][j]==target){
                 return true;
-            }
-            else if(i<matrix.size()-1&&target>=matrix[i+1][j]){
+            }else if(i<matrix.size()-1 && target>=matrix[i+1][j]){
                 i++;
-            }
-            
-            else if(j<matrix[0].size()-1&&target>=matrix[i][j+1]){
+            }else if(j<matrix[0].size()-1 && target>=matrix[i][j+1]){
                 j++;
-            }
-            else {
+            }else{
                 i++;
                 j++;
             }
